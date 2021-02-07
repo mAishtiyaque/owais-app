a23e74db4b485d78dcee533a2ba212a5e1fe4616
Md Ishtiyaque Ahmad <69048853+mAishtiyaque@users.noreply.github.com>
2021-02-07 09:35:27 +0530

    5000

--- olama.py
+++ olama.py
@@ -6,4 +6,4 @@ def index():
     return render_template('index.html')
 if __name__=="__main__":
     app.run(host=os.getenv('IP', '0.0.0.0'), 
-            port=int(os.getenv('PORT', 4444)))
+            port=int(os.getenv('PORT', 5000)))
